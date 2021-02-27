68955df8033068b00daae59d1709fec608215270
leandroandrade <leandro.andrade2588@gmail.com>
2021-02-27 15:51:15 -0300

    integration test

---
+++ test/integration/entrypoints/rest/users-controller.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+
+import app from '../../../../src/configurations/app';
+
+describe('simple error test', () => {
+    it('should return error', async (done) => {
+        const { status, body } = await request(app).get('/api/v1/users');
+
+        expect(status).toBe(200);
+
+        const { results } = body;
+        expect(results.length).toBe(1);
+
+        done();
+    });
+});
